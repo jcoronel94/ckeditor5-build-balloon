59e5ba7c557784ee70d7159fc3ed58abee65aef9
jonathan.coronel@guycarp.com <jonathan.coronel@guycarp.com>
2020-01-31 10:38:14 -0500

    custom build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,13 +5,15 @@
 
 // The editor creator to use.
 import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
-
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
@@ -19,6 +21,7 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
@@ -28,17 +31,22 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
 // Plugins to include in the build.
 BalloonEditor.builtinPlugins = [
+	Alignment,
 	Essentials,
+	Base64UploadAdapter,
 	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
 	BlockQuote,
+	CodeBlock,
 	CKFinder,
 	EasyImage,
 	Heading,
@@ -46,6 +54,7 @@ BalloonEditor.builtinPlugins = [
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
+	ImageResize,
 	ImageUpload,
 	Indent,
 	Link,
@@ -54,12 +63,17 @@ BalloonEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	TextTransformation,
+	SimpleUploadAdapter
 ];
 
 // Editor configuration.
 BalloonEditor.defaultConfig = {
 	toolbar: {
+		alignment: {
+            options: [ 'left', 'center', 'right', 'justify' ]
+        },
 		items: [
 			'heading',
 			'|',
@@ -74,8 +88,11 @@ BalloonEditor.defaultConfig = {
 			'|',
 			'imageUpload',
 			'blockQuote',
+			'codeBlock',
 			'insertTable',
 			'mediaEmbed',
+			'|',
+			'alignment',
 			'undo',
 			'redo'
 		]
@@ -84,8 +101,22 @@ BalloonEditor.defaultConfig = {
 		toolbar: [
 			'imageStyle:full',
 			'imageStyle:side',
+			'imageStyle:alignLeft',
+			'imageStyle:Center',
+			'imageStyle:alignRight',
 			'|',
 			'imageTextAlternative'
+		],
+		styles: [
+			// This option is equal to a situation where no style is applied.
+			'full',
+			'side',
+			'alignCenter',
+			// This represents an image aligned to the left.
+			'alignLeft',
+
+			// This represents an image aligned to the right.
+			'alignRight'
 		]
 	},
 	table: {
@@ -94,6 +125,33 @@ BalloonEditor.defaultConfig = {
 			'tableRow',
 			'mergeTableCells'
 		]
+	},
+	typing: {
+		transformations: {
+			remove: [
+				// Do not use the transformations from the
+				// 'symbols' and 'quotes' groups.
+				'quotes',
+				'quotesPrimaryEnGb',
+				'quotesSecondaryEnGb',
+				'quotesPrimaryPl',
+				'quotesSecondaryPl',
+			],
+
+			extra: [
+				// Add some custom transformations – e.g. for emojis.
+				{ from: ':)', to: '🙂' },
+				{ from: ':+1:', to: '👍' },
+				{ from: ':tada:', to: '🎉' },
+
+				// Finally, you can define `to` as a callback.
+				// This (naive) rule will auto-capitalize the first word after a period.
+				{
+					from: /(\. )([a-z])$/,
+					to: matches => [ null, matches[ 1 ].toUpperCase() ]
+				}
+			],
+		}
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
